18d4570eaab35d2ad304811e027f285fe303a79c
iHype <42493144+iHype@users.noreply.github.com>
2018-09-04 14:09:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -270,7 +270,7 @@ if(cmd === `${prefix}say`) {
     if (args.includes("@here")) {
         args.splice(/@here/g,"@heré");
     }     
-args.splice( args.indexOf('@'), 0 );
+args.splice( args.indexOf('<@&481534348303007754>'), 1 );
      let argsSay = args.join(" ");  
     message.channel.send(argsSay);
     console.log(message.author + " just made the bot say:\n" + argsSay);

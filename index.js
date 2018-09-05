42d182bbd884685e2a53ae9c7792f89cb4056ada
iHype <42493144+iHype@users.noreply.github.com>
2018-09-06 01:24:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -282,8 +282,9 @@ if(cmd == `${prefix}translate`) {
     
     if(cmd == `{prefix}fixyt` && message.author.id == "430447525800181762") {
     let Yt = "<@&333690877044064259>";
-            message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(Yt, {
+  let fixit = message.guild.roles.find('id', "<@&333690877044064259>");           
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(fixit, {
             SEND_MESSAGES: true,
             ADD_REACTIONS: true
           });

7d75bd78c51fae902994274099539b219036825b
iHype <42493144+iHype@users.noreply.github.com>
2018-09-07 21:26:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ translate(translateArg + "", {to: tragetLanguage + ""}).then(res => {
      message.channel.send(unscramble(toUnscramble).join(" ").toString())
      }}
   if(cmd === `${prefix}blacklistword`) {
-  if(message.author.id == "299495028756054016" || message.author.id == "430447525800181762"){
+  if(message.author.id == "299495028756054016" || message.author.id == "430447525800181762" || message.author.id == "341602886935117835" || message.author.id == "393412463153905675"){
   let wordToBlacklist = args[0];
       filter.addWords(wordToBlacklist);
   }

926b3421b5843dbc25c04baaa075f3895601e372
iHype <42493144+iHype@users.noreply.github.com>
2018-09-14 18:56:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -131,8 +131,7 @@ translate(translateArg + "", {to: tragetLanguage + ""}).then(res => {
     if(message.author.id == "299495028756054016" || message.author.id == "437254213689540610" || message.author.id == "430447525800181762" || message.author.id == "341602886935117835" || message.author.id == "393412463153905675"){
         try{
     filter.removeWords(args.join(" ").toString());
-    let confirmWhitelist = message.channel.send(message.author + ": succesfully whitelisted word(s): " + args);
-        confirmWhitelist.delete(3000);
+        message.channel.send(message.author + ": succesfully whitelisted word(s): " + args);
         }catch(err){
         message.channel.send("Error occurred\n" + err);
             console.log(err)
@@ -147,8 +146,7 @@ translate(translateArg + "", {to: tragetLanguage + ""}).then(res => {
   let wordsToBlacklist = args;
       filter.addWords(wordsToBlacklist);
   }
-      let confirmBlacklist = message.channel.send(message.author + ": successfully blacklisted word(s): " + args);
-      confirmBlacklist.delete(3000);
+message.channel.send(message.author + ": successfully blacklisted word(s): " + args);
   }
 
   if(cmd === `${prefix}logs`) {

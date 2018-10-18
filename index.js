c6833f19d2a13ac1099cee35f3d832ed750a6f44
iHype <42493144+iHype@users.noreply.github.com>
2018-10-18 13:13:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -225,13 +225,12 @@ bot.on("message", async message => {
             }
         };
         function callback(error, response, body) {
-            if (!error && response.statusCode == 200) {
                 var info = JSON.parse(body);
                 let embedFortnite = new Discord.RichEmbed()
                 .setTitle(`Stats of ${name} on ${platform}`)
-                .setDescription(`**Matches played: ${info.lifeTimeStats[7].value}**`);
-                message.channel.send(embedFortnite)
-            }
+               .setDescription(`**Matches played: ${info.lifeTimeStats[7].value}**`);
+            message.channel.send(embedFortnite)
+            
         }
     }
     
